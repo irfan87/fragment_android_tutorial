d2b37a8f7727f0faf8f54056e62858d576cd2bab
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-18 15:59:18 +0800

    cleanup FirstFragment and SecondFragment class

--- app/src/main/java/com/irfanshukri203/fragmenttutorial/FirstFragment.kt
+++ app/src/main/java/com/irfanshukri203/fragmenttutorial/FirstFragment.kt
@@ -1,11 +1,6 @@
 package com.irfanshukri203.fragmenttutorial
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass.
