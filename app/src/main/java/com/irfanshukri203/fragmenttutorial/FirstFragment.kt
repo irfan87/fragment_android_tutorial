bc0cda4907025905d5b929a2e94d38d61537fa77
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-18 15:56:28 +0800

    navigate page with fragment

---
+++ app/src/main/java/com/irfanshukri203/fragmenttutorial/FirstFragment.kt
@@ -0,0 +1,17 @@
+package com.irfanshukri203.fragmenttutorial
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_first.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+}
--- app/src/main/java/com/irfanshukri203/fragmenttutorial/MainActivity.kt
+++ app/src/main/java/com/irfanshukri203/fragmenttutorial/MainActivity.kt
@@ -2,10 +2,35 @@ package com.irfanshukri203.fragmenttutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayoutFragment, firstFragment)
+            commit()
+        }
+
+        btnFirstFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayoutFragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        btnSecondFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.frameLayoutFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
